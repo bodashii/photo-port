660ceb9e8070724a89a4b593a3df19c4c449722d
maddnty <nzkb24@gmail.com>
2022-05-06 18:22:11 -0500

    added formState

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState} from "react";
 
 function ContactForm() {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+    const { name, email, message } =formState;
 
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+        console.log(formState);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
+    
     // JSX
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 {/* name input */}
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" onChange={handleChange} defaultValue={name} />
                 </div>
                 {/* email input */}
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" onChange={handleChange} defaultValue={email} />
                 </div>
                 {/* message text area */}
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" rows="5" onChange={handleChange} defaultValue={message} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
