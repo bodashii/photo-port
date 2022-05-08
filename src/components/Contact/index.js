d6aab29e9e3ab8b1c8d5ed3c8b65e494029b0311
maddnty <nzkb24@gmail.com>
2022-05-07 21:16:41 -0500

    converted to an spa

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState} from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({
@@ -7,12 +8,31 @@ function ContactForm() {
         message: ''
     });
     const { name, email, message } =formState;
+    const [errorMessage, setErrorMessage] = useState('');
 
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-        console.log(formState);
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if(!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value })
+            console.log(formState);
+        }
     }
-
+    
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
@@ -39,6 +59,11 @@ function ContactForm() {
                     <label htmlFor="message">Message:</label>
                     <textarea name="message" rows="5" onChange={handleChange} defaultValue={message} />
                 </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
